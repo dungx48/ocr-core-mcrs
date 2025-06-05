08ea501576b2c9157a6a892deacb544f62a5243a
vdungx <vdungx@vdungxs-MacBook-Pro.local>
2025-06-05 17:02:09 +0700

    [dungpv][feat] run text detection

--- app/src/controller/text_detection_controller.py
+++ app/src/controller/text_detection_controller.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Request
 
 from src.service.text_detection_service import TextDetectionService
 from src.dto.request.text_detect_request import TextDetectRequest
+from src.common.base.structural.response_object import ResponseObject
+
 text_detection_router = APIRouter(
     prefix="/text-detect",
     tags=["TextDetection"]
@@ -9,12 +11,13 @@ text_detection_router = APIRouter(
 text_detection_service = TextDetectionService()
 
 @text_detection_router.post("/", response_model=dict, summary="Detect text from image")
-def detect_text(request: TextDetectRequest):
+def detect_text(request: Request, input: TextDetectRequest) -> ResponseObject:
     """
     POST /text-detect/
     Body JSON: { "image_path": "đường_dẫn_đến_ảnh.jpg" }
     """
     try:
-        pass
+        data_result = text_detection_service.text_detection_by_craft(input.image_path)
+        return ResponseObject().success(data_result=data_result, path=request.url.__str__())
     except Exception as e:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+        return ResponseObject().error(error=str(e), path=request.url.__str__())
